0bc5ab39bb017d033cd6891f73d5b4d672b9aec2
Nirbhay <68394007+Nirbhayadh@users.noreply.github.com>
2023-04-17 15:45:47 +0545

    code optimized

--- lab2i.py
+++ lab2i.py
@@ -3,15 +3,9 @@ from OpenGL.GLUT import *
 from math import ceil
 
 windowSize = (500, 500)
-division = 30
+division = 15
 x_stepsize = 2/division
 y_stepsize = 2/division
-print(x_stepsize)
-# (1,1) to (19,19)
-
-
-def roundPartial(value, resolution):
-    return round(value / resolution) * resolution
 
 
 def Plot(x, y, g):
@@ -21,14 +15,11 @@ def Plot(x, y, g):
         glPointSize(3.0)
     else:
         glColor3f(1.0, 0.0, 0.0)
-        glPointSize(8.0)
+        glPointSize(5.0)
 
     glBegin(GL_POINTS)
     x = -1+x*x_stepsize
-    # y = -1+y*y_stepsize
-    y = roundPartial(-1+y*y_stepsize, y_stepsize)
-    # x = ceil(-1+x*x_stepsize)
-    # y = ceil(-1+y*y_stepsize)
+    y = -1+y*y_stepsize
     glVertex2f(x, y)
     glEnd()
 
@@ -39,13 +30,22 @@ def Points():
             Plot(i, j, 'p')
 
 
+def Actual(p1, p2):
+    glColor3f(0.0, 1.0, 0.0)
+    glPointSize(3.0)
+    glBegin(GL_LINES)
+    glVertex2f(-1+p1[0]*x_stepsize, -1+p1[1]*y_stepsize)
+    glVertex2f(-1+p2[0]*x_stepsize, -1+p2[1]*y_stepsize)
+    glEnd()
+
+
 def Line():
 
-    p1 = (1, 1)
-    p2 = (11, 10)
+    p2 = (1, 1)
+    p1 = (6, 10)
+    Actual(p1, p2)
     dx = p2[0]-p1[0]
     dy = p2[1]-p1[1]
-    m = (p2[1]-p1[1])/(p2[0]-p1[0])
     if (abs(dx) > abs(dy)):
         stepsize = abs(dx)
     else:
@@ -54,13 +54,18 @@ def Line():
     y_inc = dy/stepsize
     x = p1[0]
     y = p1[1]
+    print(x, y)
+    print("lund")
+    print(x_inc, y_inc)
     Plot(x, y, 'l')
-
     iteration = 0
     while (iteration != stepsize):
         x = x + x_inc
         y = y + y_inc
-        Plot(x, y, 'l')
+
+        print(x, y)
+        print(ceil(x), ceil(y))
+        Plot(ceil(x), ceil(y), 'l')
         iteration = iteration+1
 
 
@@ -69,7 +74,6 @@ def DDA():
     glClear(GL_COLOR_BUFFER_BIT)
     Points()
     Line()
-
     glFlush()
 
 
@@ -77,8 +81,8 @@ def main():
     glutInit()
     glutInitDisplayMode(GLUT_RGB)
     glutInitWindowSize(windowSize[0], windowSize[1])
-    glutInitWindowPosition(100, 100)
-    glutCreateWindow("Nirbhay Adhikari (02)")
+    glutInitWindowPosition(300, 100)
+    glutCreateWindow("Nirbhay Adhikari (02) DDA")
     glutDisplayFunc(DDA)
     glutMainLoop()
 
